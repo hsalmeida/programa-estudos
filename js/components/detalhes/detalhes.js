3c7ce17ac8f9008b2cd98a52c5dec68fed08eb7c
hsalmeida <hsalmeida@gmail.com>
2016-03-14 17:16:17 -0300

    detalhes

--- js/components/detalhes/detalhes.js
+++ js/components/detalhes/detalhes.js
@@ -1,6 +1,7 @@
 angular.module('estudos').controller('DetalhesController', ['$scope', '$rootScope', '$state', '$stateParams', 'Assuntos',
     '$modal',
     function ($scope, $rootScope, $state, $stateParams, Assuntos, $modal) {
+        $scope.relevante = true;
         $scope.initDet = function () {
             waitingDialog.show();
             Assuntos.getById($stateParams.materia).then(function (materia) {
@@ -14,16 +15,26 @@ angular.module('estudos').controller('DetalhesController', ['$scope', '$rootScop
             $modal
                 .open({
                     templateUrl: 'confirmarExclusao.html',
-                    controller: function ($scope) {
+                    controller: function ($scope, parentScope) {
+                        $scope.relevante = true;
                         $scope.cancelarExclusao = function () {
                             $scope.$dismiss();
                         };
 
                         $scope.confirmarExclusao = function () {
+                            parentScope.relevante = $scope.relevante;
                             $scope.$close(true);
                         };
+                    },
+                    resolve: {
+                        parentScope: function(){
+                            return $scope;
+                        }
                     }
                 }).result.then(function () {
+                    if($scope.relevante) {
+
+                    }
                     $scope.materiaMae.materias[$stateParams.indice].datas.splice(indice, 1);
                     $scope.materiaMae.$saveOrUpdate().then(function () {
                         $state.reload();
