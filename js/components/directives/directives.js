9434f41cc1f008401c0598d9fa9466aea00e0d72
hsalmeida <hsalmeida@gmail.com>
2016-03-17 11:39:49 -0300

    loading

--- js/components/directives/directives.js
+++ js/components/directives/directives.js
@@ -1,36 +1,63 @@
 angular.module('estudos')
-.directive("checkboxGroup", function () {
-    return {
-        restrict: "A",
-        link: function (scope, elem, attrs) {
-            // Determine initial checked boxes
-            if (scope.array.indexOf(
-                    scope.$parent.materia._id.$oid + "#" + scope.$parent.materia.materias.indexOf(scope.assunto)
-                ) !== -1) {
-                elem[0].checked = true;
-            }
-            // Update array on click
-            elem.bind('click', function () {
-                var index =
-                    scope.array.indexOf(
-                        scope.$parent.materia._id.$oid + "#" + scope.$parent.materia.materias.indexOf(scope.assunto)
-                    );
-                // Add if checked
-                if (elem[0].checked) {
-                    if (index === -1)
-                        scope.array.push(
-                            scope.$parent.materia._id.$oid + "#" + scope.$parent.materia.materias.indexOf(scope.assunto)
-                        );
+    .directive(
+    "mAppLoading",
+    function ($animate) {
+        // Return the directive configuration.
+        return ({
+            link: link,
+            restrict: "C"
+        });
+        // I bind the JavaScript events to the scope.
+        function link(scope, element, attributes) {
+            // Due to the way AngularJS prevents animation during the bootstrap
+            // of the application, we can't animate the top-level container; but,
+            // since we added "ngAnimateChildren", we can animated the inner
+            // container during this phase.
+            // --
+            // NOTE: Am using .eq(1) so that we don't animate the Style block.
+            $animate.leave(element.children().eq(1)).then(
+                function cleanupAfterAnimation() {
+                    // Remove the root directive element.
+                    element.remove();
+                    // Clear the closed-over variable references.
+                    scope = element = attributes = null;
                 }
-                // Remove if unchecked
-                else {
-                    if (index !== -1) scope.array.splice(index, 1);
-                }
-                // Sort and update DOM display
-                scope.$apply(scope.array.sort(function (a, b) {
-                    return a - b
-                }));
-            });
+            );
         }
     }
-});
+)
+    .directive("checkboxGroup", function () {
+        return {
+            restrict: "A",
+            link: function (scope, elem, attrs) {
+                // Determine initial checked boxes
+                if (scope.array.indexOf(
+                        scope.$parent.materia._id.$oid + "#" + scope.$parent.materia.materias.indexOf(scope.assunto)
+                    ) !== -1) {
+                    elem[0].checked = true;
+                }
+                // Update array on click
+                elem.bind('click', function () {
+                    var index =
+                        scope.array.indexOf(
+                            scope.$parent.materia._id.$oid + "#" + scope.$parent.materia.materias.indexOf(scope.assunto)
+                        );
+                    // Add if checked
+                    if (elem[0].checked) {
+                        if (index === -1)
+                            scope.array.push(
+                                scope.$parent.materia._id.$oid + "#" + scope.$parent.materia.materias.indexOf(scope.assunto)
+                            );
+                    }
+                    // Remove if unchecked
+                    else {
+                        if (index !== -1) scope.array.splice(index, 1);
+                    }
+                    // Sort and update DOM display
+                    scope.$apply(scope.array.sort(function (a, b) {
+                        return a - b
+                    }));
+                });
+            }
+        }
+    });
