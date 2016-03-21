5d59a089102bfce1d13ba204fc089b89d2d9cbe9
hsalmeida <hsalmeida@gmail.com>
2016-03-21 16:55:45 -0300

    menu e editar assuntos

--- js/components/assuntos/assuntos.js
+++ js/components/assuntos/assuntos.js
@@ -1,5 +1,5 @@
-angular.module('estudos').controller('AssuntosController', ['$scope', '$rootScope', '$state', 'Assuntos',
-    function ($scope, $rootScope, $state, Assuntos) {
+angular.module('estudos').controller('AssuntosController', ['$scope', '$rootScope', '$state', 'Assuntos', '$modal',
+    function ($scope, $rootScope, $state, Assuntos, $modal) {
         $scope.assuntosInit = function () {
             waitingDialog.show();
 
@@ -7,7 +7,6 @@ angular.module('estudos').controller('AssuntosController', ['$scope', '$rootScop
                 $scope.materiasUnificadas = assuntos;
                 waitingDialog.hide();
             });
-
         };
 
         $scope.editar = function (assunto) {
@@ -15,6 +14,66 @@ angular.module('estudos').controller('AssuntosController', ['$scope', '$rootScop
         };
 
         $scope.excluir = function (assunto) {
+            $modal
+                .open({
+                    templateUrl: 'exclusao.html',
+                    controller: function ($scope) {
+                        $scope.cancelarExclusao = function () {
+                            $scope.$dismiss();
+                        };
 
+                        $scope.confirmarExclusao = function () {
+                            assunto.$remove().then(function () {
+                                $scope.$close(true);
+                            });
+                        };
+                    }
+                }).result.then(function () {
+                    $state.reload();
+                }, function () {
+
+                });
+        };
+
+        $scope.criarAssunto = function () {
+            $modal
+                .open({
+                    templateUrl: 'criar-assunto.html',
+                    controller: function ($scope, assuntosDB) {
+
+                        $scope.initCriar = function () {
+                            $scope.assunto = new assuntosDB();
+                            $scope.assunto.assunto = "";
+                            $scope.assunto.geral = {
+                                "total": 0,
+                                "acertos": 0,
+                                "aproveitamento": 0
+                            };
+                            $scope.assunto.materias = [];
+                            $scope.assunto.status = "";
+                            $scope.assunto.qtdMaterias = 0;
+                            $scope.assunto.arrayStatus = [100, 0, 0, 0];
+                        };
+
+                        $scope.cancelarCriacao = function () {
+                            $scope.$dismiss();
+                        };
+
+                        $scope.confirmarCriacao = function () {
+                            if($scope.assunto.assunto) {
+                                $scope.assunto.$saveOrUpdate().then(function () {
+                                    $scope.$close(true);
+                                });
+                            }
+                        };
+                    },
+                    resolve: {
+                        assuntosDB: function () {
+                            return Assuntos;
+                        }
+                    }
+                }).result.then(function () {
+                    $state.reload();
+                }, function () {});
         };
     }]);
