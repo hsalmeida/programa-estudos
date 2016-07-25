54f754ccb6984c28475c12df37c1f3c1eed5734d
hsalmeida <hsalmeida@gmail.com>
2016-07-25 14:33:04 -0300

    atualizacao das cores

--- js/estudos.js
+++ js/estudos.js
@@ -1,6 +1,7 @@
 angular.module("estudos", [
     'ui.router', 'ngResource', 'mongolabResourceHttp', 'mwl.calendar',
-    'ngCookies', 'ngAnimate', 'ui.bootstrap', 'chart.js', 'dndLists'
+    'ngCookies', 'ngAnimate', 'ui.bootstrap', 'chart.js', 'dndLists',
+    'colorpicker.module'
 ])
     .constant('MONGOLAB_CONFIG', {API_KEY: 'YXgR-q92vuVCKlSm-ji3nplDTE7rHIQh', DB_NAME: 'ltdb'})
     .config(function (calendarConfig) {
