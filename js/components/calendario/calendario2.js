c8ebbff5fb1421fa5d16d06a98b5d1af0c90a91c
hsalmeida <hsalmeida@gmail.com>
2016-08-09 15:39:25 -0300

    inicio do novo calendario

--- js/components/calendario/calendario2.js
+++ js/components/calendario/calendario2.js
@@ -17,6 +17,49 @@ angular.module('estudos').controller('Calendario2Controller', ['$scope', '$rootS
                     prev: 'btn'
                 }
             };
+            waitingDialog.show("Aguarde. Carregando calendário");
+            $scope.events = [];
+            $scope.assuntos = [];
+            var ativos = {
+                "ativo": true
+            };
+            Assuntos.query(ativos).then(function (assuntos) {
+                $scope.assuntos = assuntos;
+                angular.forEach(assuntos, function (assunto, assuntoIndex) {
+                    assunto.horasTotal = 0;
+                    assunto.minutosTotal = 0;
+                    angular.forEach(assunto.materias, function(materia, materiaIndex){
+
+                        angular.forEach(materia.datas, function(data, dataIndex){
+
+                            var textStatus = data.status ? data.status : "em andamento";
+                            var tempoData = data.tempo === 0 ? 2 : new Date(data.tempo).getHours();
+                            var minutoData = data.tempo === 0 ? 0 : new Date(data.tempo).getMinutes();
+                            minutoData = minutoData === 0 ? "" : minutoData;
+
+                            assunto.horasTotal += tempoData;
+                            assunto.minutosTotal += Number(minutoData);
+
+                            var inicioData = new Date(data.data);
+                            inicioData.setTime(inicioData.getTime() - (tempoData * 60 * 60 * 1000));
+                            var fimData = new Date(inicioData);
+                            fimData.setTime(fimData.getTime() + (tempoData * 60 * 60 * 1000));
+
+                            var evento = {
+                                title: '•' + materia.topico + ': •' + materia.texto + ' (' + textStatus + ')',
+                                start: inicioData,
+                                end: fimData,
+                                editable: false,
+                                color: assunto.cor
+                            };
+                            $scope.events.push(evento);
+                        });
+                    });
+                    assunto.horasTotal += assunto.minutosTotal !== 0 ? assunto.minutosTotal / 60 : 0;
+                });
+            });
+            $scope.eventSources = [$scope.events];
+            waitingDialog.hide();
         };
 
     }]);
