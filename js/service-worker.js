465a0c889c5118af72a6d219579c2cc7d01c0b33
hsalmeida <hsalmeida@gmail.com>
2016-08-31 23:33:12 -0300

    service worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -9,21 +9,21 @@ var filesToCache = [
     '/programa-estudos/img/ic_school_48pt_3x.png'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         })
     );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
                 if (key !== cacheName) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
@@ -32,27 +32,43 @@ self.addEventListener('activate', function(e) {
         })
     );
 });
-/*
-self.addEventListener('fetch', function(e) {
+
+self.addEventListener('fetch', function (e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
-    var dataUrl = 'https://publicdata-weather.firebaseio.com/';
-    if (e.request.url.indexOf(dataUrl) === 0) {
-        e.respondWith(
-            fetch(e.request)
-                .then(function(response) {
-                    return caches.open(dataCacheName).then(function(cache) {
-                        cache.put(e.request.url, response.clone());
-                        console.log('[ServiceWorker] Fetched&Cached Data');
-                        return response;
-                    });
-                })
-        );
-    } else {
-        e.respondWith(
-            caches.match(e.request).then(function(response) {
-                return response || fetch(e.request);
-            })
-        );
-    }
+    e.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
 });
-*/
+
+/*
+ self.addEventListener('fetch', function(e) {
+ console.log('[ServiceWorker] Fetch', e.request.url);
+ var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+ if (e.request.url.indexOf(dataUrl) === 0) {
+ e.respondWith(
+ fetch(e.request)
+ .then(function(response) {
+ return caches.open(dataCacheName).then(function(cache) {
+ cache.put(e.request.url, response.clone());
+ console.log('[ServiceWorker] Fetched&Cached Data');
+ return response;
+ });
+ })
+ );
+ } else {
+ e.respondWith(
+ caches.match(e.request).then(function(response) {
+ return response || fetch(e.request);
+ })
+ );
+ }
+ });
+ */
